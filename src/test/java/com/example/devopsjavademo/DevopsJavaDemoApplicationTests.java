48c06014170820a651c3a1a6a5105b35fffaec8b
MosALs <mostaffa4444.ma@gmail.com>
2024-12-28 00:10:10 +0300

    inital commit

---
+++ src/test/java/com/example/devopsjavademo/DevopsJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsjavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
